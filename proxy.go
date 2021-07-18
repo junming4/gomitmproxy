071582a82cf884c983be462bddefb65a94d5bf9a
junming4 <2284876299@qq.com>
2021-07-18 23:22:31 +0800

    去掉日志

--- proxy.go
+++ proxy.go
@@ -474,7 +474,7 @@ func (p *Proxy) handleConnect(session *Session) error {
 			// Handshake with the local client
 			if err := tlsConn.Handshake(); err != nil {
 				// TODO: Consider adding to invalidTLSHosts? -- we should do this if this happens a couple of times in a short period of time
-				log.Error("id=%s: failed to handshake with the client: %v", session.ID(), err)
+				//log.Error("id=%s: failed to handshake with the client: %v", session.ID(), err)
 				return err
 			}
 
